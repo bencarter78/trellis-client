5859ad4e9831338571c6c44ed99263091670b5b2
Ben Carter <bencarter78@hotmail.com>
2017-07-15 11:28:51 +0100

    Adding project nav

--- src/components/nav/Project.vue
+++ src/components/nav/Project.vue
@@ -2,8 +2,86 @@
   <aside class="menu">
     <p class="menu-label">{{ project }}</p>
     <ul class="menu-list">
-      <li><a class='is-active'>Projects</a></li>
-      <li><a>Settings</a></li>
+      <li>
+        <a class='is-active'>
+          <span class="icon is-small">
+            <i class="fa fa-fw fa-tachometer has-margin-right"></i>
+          </span>
+          Overview
+        </a>
+      </li>
+
+      <li>
+        <a>
+          <span class="icon is-small">
+            <i class="fa fa-fw fa-bullseye has-margin-right"></i>
+          </span>
+          Objectives
+        </a>
+        </li>
+
+      <li>
+        <a>
+          <span class="icon is-small">
+            <i class="fa fa-fw fa-code-fork has-margin-right"></i>
+          </span>
+          Streams
+        </a>
+        </li>
+
+      <li>
+        <a>
+          <span class="icon is-small">
+            <i class="fa fa-fw fa-check-square-o has-margin-right"></i>
+          </span>
+          Tasks
+        </a>
+        </li>
+
+      <li>
+        <a>
+          <span class="icon is-small">
+            <i class="fa fa-fw fa-lightbulb-o has-margin-right"></i>
+          </span>
+          Lessons
+        </a>
+        </li>
+
+      <li>
+        <a>
+          <span class="icon is-small">
+            <i class="fa fa-fw fa-users has-margin-right"></i>
+          </span>
+          Milestones
+        </a>
+        </li>
+
+      <li>
+        <a>
+          <span class="icon is-small">
+            <i class="fa fa-fw fa-users has-margin-right"></i>
+          </span>
+          Status
+        </a>
+        </li>
+
+      <li>
+        <a>
+          <span class="icon is-small">
+            <i class="fa fa-fw fa-users has-margin-right"></i>
+          </span>
+          Risks
+        </a>
+        </li>
+
+      <li>
+        <a>
+          <span class="icon is-small">
+            <i class="fa fa-fw fa-users has-margin-right"></i>
+          </span>
+          Issues
+        </a>
+        </li>
       <li>
         <a>Manage Your Team</a>
         <ul>
