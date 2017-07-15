2fd27e6242b9c350315c00461ce94a4906fac17c
Ben Carter <bencarter78@hotmail.com>
2017-07-15 11:29:43 +0100

    Experimenting with layout

--- src/views/projects/Show.vue
+++ src/views/projects/Show.vue
@@ -5,18 +5,65 @@
     </div>
     <div class="column is-9">
       <div v-if="item">
-        <h1 class="title is-3">{{ item.name }} Projects</h1>
-        <div v-if="item.projects">
-          <trellis-card-projects :items="item.projects"></trellis-card-projects>
-        </div>
-        <div v-else>
-            <p>
-              Looks like you have no projects for this team yet.
-            </p>
+        <nav class="level">
+          <div class="level-left">
+            <div class="level-item">
+              <p class="subtitle is-5">
+                <strong>{{ item.name }}</strong> Project Overview
+              </p>
+            </div>
+          </div>
 
-            <router-link :to="{name: 'projects.create'}" class="button is-primary has-margin-top">
-              Create Project
-            </router-link>
+          <div class="level-right">
+            <p class="level-item">
+              <i class="fa fa-calendar has-margin-right"></i>
+              20/10/17
+            </p>
+            <p class="level-item">
+              <a class="button is-outlined is-success">
+                On Track
+              </a>
+            </p>
+          </div>
+        </nav>
+
+        <div class="columns">
+          <div class="column is-6">
+            <trellis-card>
+              <div slot="title">Objectives</div>
+
+              <div slot="content">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam error porro libero totam harum eum sequi aliquam molestiae illum provident laboriosam perspiciatis maxime minus ullam, sint quasi, voluptatem natus adipisci.
+              </div>
+            </trellis-card>
+          </div>
+
+          <div class="column is-6">
+            <trellis-card>
+              <div slot="title">Upcoming Tasks</div>
+
+              <div slot="content">
+                <table class="table">
+                  <thead>
+                    <tr>
+                      <th>Task</th>
+                      <th>Due</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>Some Task</td>
+                      <td>2 days</td>
+                    </tr>
+                    <tr>
+                      <td>Some Other Task</td>
+                      <td>20/10/17</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </trellis-card>
+          </div>
         </div>
       </div>
     </div>
